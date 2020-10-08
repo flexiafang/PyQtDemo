968841c18595388b25c6527b287926f58e9bfc8f
flexiafang <1208689216@qq.com>
2020-10-08 20:04:47 +0800

    QLineEdit

---
+++ src/controls/QLineEditEchoMode.py
@@ -0,0 +1,44 @@
+import sys
+
+from PyQt5.QtWidgets import QWidget, QFormLayout, QLineEdit, QApplication
+
+
+class QLineEditEchoMode(QWidget):
+    def __init__(self):
+        super(QLineEditEchoMode, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('文本输入框演示')
+
+        formLayout = QFormLayout()
+
+        normalLineEdit = QLineEdit()
+        noEchoLineEdit = QLineEdit()
+        passwordLineEdit = QLineEdit()
+        passwordEchoOnEditLineEdit = QLineEdit()
+
+        formLayout.addRow('Normal', normalLineEdit)
+        formLayout.addRow('NoEcho', noEchoLineEdit)
+        formLayout.addRow('Password', passwordLineEdit)
+        formLayout.addRow('PassWordEchoOnEdit', passwordEchoOnEditLineEdit)
+
+        # placeholder text
+        normalLineEdit.setPlaceholderText("Normal")
+        noEchoLineEdit.setPlaceholderText("NoEcho")
+        passwordLineEdit.setPlaceholderText("Password")
+        passwordEchoOnEditLineEdit.setPlaceholderText("PasswordEchoOnEdit")
+
+        normalLineEdit.setEchoMode(QLineEdit.Normal)
+        noEchoLineEdit.setEchoMode(QLineEdit.NoEcho)
+        passwordLineEdit.setEchoMode(QLineEdit.Password)
+        passwordEchoOnEditLineEdit.setEchoMode(QLineEdit.PasswordEchoOnEdit)
+
+        self.setLayout(formLayout)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = QLineEditEchoMode()
+    main.show()
+    sys.exit(app.exec_())
